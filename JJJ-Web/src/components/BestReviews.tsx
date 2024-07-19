dcc910f3dc2e6e32b109742d602e5aa08db18d38
Ted <shinsuengju1@gmail.com>
2024-07-19 12:29:00 +0900

    2024.07.19 updated folder structure, Search.tsx, BestReviews.tsx

--- JJJ-Web/src/components/BestReviews.tsx
+++ JJJ-Web/src/components/BestReviews.tsx
@@ -1,5 +1,95 @@
-import '../styles/BestReviews.module.css';
-
+import styles from '../styles/components/BestReviews.module.css';
+import exampleImg from '../assets/images/cars.jpg';
 export default function BestReviews() {
-  return <div>BestReviews</div>;
+  return (
+    <section className={styles.best__reviews}>
+      <h3>베스트 리뷰</h3>
+      <div className={styles.grid__items}>
+        <div className={styles.item__container}>
+          <a href='#' className={styles.item}>
+            <img src={exampleImg} alt='item' className={styles.item__img} />
+            <div className={styles.item__descriptions}>
+              <div className={styles.item__name}>제이스랜드 출동 구조대</div>
+              <div className={styles.item__infos}>
+                <div className={styles.item__price}>26,700원</div>
+                <div className={styles.rating__star__container}>
+                  <div className={styles.stars}>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                  </div>
+                  <div className={styles.rating__count}>(256)</div>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className='item__container'>
+          <a href='#' className={styles.item}>
+            <img src={exampleImg} alt='item' className={styles.item__img} />
+            <div className={styles.item__descriptions}>
+              <div className={styles.item__name}>제이스랜드 출동 구조대</div>
+              <div className={styles.item__infos}>
+                <div className={styles.item__price}>26,700원</div>
+                <div className={styles.rating__star__container}>
+                  <div className={styles.stars}>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                  </div>
+                  <div className={styles.rating__count}>(256)</div>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className='item__container'>
+          <a href='#' className={styles.item}>
+            <img src={exampleImg} alt='item' className={styles.item__img} />
+            <div className={styles.item__descriptions}>
+              <div className={styles.item__name}>제이스랜드 출동 구조대</div>
+              <div className={styles.item__infos}>
+                <div className={styles.item__price}>26,700원</div>
+                <div className={styles.rating__star__container}>
+                  <div className={styles.stars}>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                  </div>
+                  <div className={styles.rating__count}>(256)</div>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className='item__container'>
+          <a href='#' className={styles.item}>
+            <img src={exampleImg} alt='item' className={styles.item__img} />
+            <div className={styles.item__descriptions}>
+              <div className={styles.item__name}>제이스랜드 출동 구조대</div>
+              <div className={styles.item__infos}>
+                <div className={styles.item__price}>26,700원</div>
+                <div className={styles.rating__star__container}>
+                  <div className={styles.stars}>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                    <i className='fa-solid fa-star'></i>
+                  </div>
+                  <div className={styles.rating__count}>(256)</div>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
--- JJJ-Web/src/components/Header.tsx
+++ JJJ-Web/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import '../styles/Header.module.css';
+import styles from '../styles/components/Header.module.css';
 
 export default function Header() {
   return (
