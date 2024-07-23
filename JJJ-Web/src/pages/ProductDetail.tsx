d9fac029443ef5169b1e1cadf8306847b78c7148
Ted <shinsuengju1@gmail.com>
2024-07-23 19:38:44 +0900

    processing ProductDetail

--- JJJ-Web/src/pages/ProductDetail.tsx
+++ JJJ-Web/src/pages/ProductDetail.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styles from '../styles/pages/ProductDetail.module.css';
 import ImageTabs from '../muiComponents/productDetail/LabTabs';
 
 import StarRateIcon from '@mui/icons-material/StarRate';
@@ -11,26 +12,28 @@ export default function ProductDetail() {
   const [count, setCount] = useState(1);
   return (
     <div className='page__margin'>
-      <ImageTabs />
-      <div>
-        <div className='details'>
-          <p>상품명</p>
-          <p>가격</p>
-          <div>
-            <StarRateIcon />
-            <span>4.8 (12)</span>
+      <div className={styles.product__container}>
+        <ImageTabs />
+        <div className={styles.detail__right}>
+          <div className={styles.details}>
+            <p>상품명</p>
+            <p>가격</p>
+            <div>
+              <StarRateIcon />
+              <span>4.8 (12)</span>
+            </div>
           </div>
-        </div>
-        <div className='wrapper'>
-          <div className='counter'>
-            <Button variant='contained'>-</Button>
-            <span>{count}</span>
-            <Button variant='contained'>+</Button>
+          <div className='wrapper'>
+            <div className='counter'>
+              <Button variant='contained'>-</Button>
+              <span>{count}</span>
+              <Button variant='contained'>+</Button>
+            </div>
+            <ShoppingCartIcon sx={{ fontSize: '50px' }} />
+            <FavoriteBorderIcon />
           </div>
-          <ShoppingCartIcon sx={{ fontSize: '50px' }} />
-          <FavoriteBorderIcon />
+          <Button variant='contained'>구매하기</Button>
         </div>
-        <Button variant='contained'>구매하기</Button>
       </div>
     </div>
   );
