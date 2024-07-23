f4966cc572e2af1c570260ce7341ae88a0953fb0
Ted <shinsuengju1@gmail.com>
2024-07-23 15:10:33 +0900

    Home에 display flex 추가, 베스트리뷰 지윤님과 코드 맞춤, 제품상세페이지 추가, 전역 styles 추가

--- JJJ-Web/src/components/BestReview.tsx
+++ JJJ-Web/src/components/BestReview.tsx
@@ -1,27 +1,30 @@
 import exampleImg from '../assets/images/cars.jpg';
+import StarRateIcon from '@mui/icons-material/StarRate';
 import styles from '../styles/components/BestReview.module.css';
+import { Scale } from '@mui/icons-material';
+import { NavLink } from 'react-router-dom';
 export default function BestReview() {
   return (
-    <div className='item__container'>
-      <a href='#' className={styles.item}>
+    <div className={styles.item__container}>
+      <NavLink to='/productDetail' className={styles.item}>
         <img src={exampleImg} alt='item' className={styles.item__img} />
         <div className={styles.item__descriptions}>
-          <div className='item__name'>제이스랜드 출동 구조대</div>
+          <div className={styles.item__title}>제이스랜드 출동 구조대</div>
           <div className={styles.item__infos}>
             <div className={styles.item__price}>26,700원</div>
             <div className={styles.rating__star__container}>
               <div className={styles.stars}>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
+                <StarRateIcon fontSize='small' />
+                <StarRateIcon fontSize='small' />
+                <StarRateIcon fontSize='small' />
+                <StarRateIcon fontSize='small' />
+                <StarRateIcon fontSize='small' />
               </div>
               <div className={styles.rating__count}>(256)</div>
             </div>
           </div>
         </div>
-      </a>
+      </NavLink>
     </div>
   );
 }
