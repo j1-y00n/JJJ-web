bfd1ebb6ece744f2640cef06bf8b6621b16bbfb7
Ted <shinsuengju1@gmail.com>
2024-07-30 19:16:07 +0900

    merge 이슈 해결

--- JJJ-Web/src/pages/CreateUsedProduct.tsx
+++ JJJ-Web/src/pages/CreateUsedProduct.tsx
@@ -1,6 +1,6 @@
-// 변지윤 
+// 변지윤
 // 중고 제품 등록
-import React from 'react'
+import React from 'react';
 import styles from '../styles/pages/CreateUsedProduct.module.css';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
@@ -15,30 +15,31 @@ const FooterWithMargin = styled(Footer)`
   background-color: red;
 `;
 
-
 export default function CreateUsedProduct() {
   // 상품상태 radio button
   const [condition, setCondition] = React.useState('새상품(미사용)');
-  
-  const handleConditionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleConditionChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setCondition((event.target as HTMLInputElement).value);
   };
 
   // 직거래 radio button
   const [transaction, setTransaction] = React.useState('가능');
-  
-  const handleTransactionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleTransactionChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setTransaction((event.target as HTMLInputElement).value);
   };
-  
+
   return (
     <>
-      <form action="">
-
+      <form action=''>
         <div className={styles.createUsedProduct__container}>
           <div className={styles.createUsedProduct__header}>중고 상품 등록</div>
           <div className={styles.createUsedProduct__inner}>
-
             {/* 상품정보 */}
             <div className={styles.create__desc__container}>
               <div className={styles.container__title}>상품정보</div>
@@ -56,24 +57,46 @@ export default function CreateUsedProduct() {
 
                   <FormControl>
                     <RadioGroup
-                      aria-labelledby="demo-controlled-radio-buttons-group"
-                      name="controlled-radio-buttons-group"
+                      aria-labelledby='demo-controlled-radio-buttons-group'
+                      name='controlled-radio-buttons-group'
                       value={condition}
                       onChange={handleConditionChange}
                     >
-                      <FormControlLabel value="새상품(미사용)" control={<Radio color='primary' />} label="새상품(미사용)" />
-                      <FormControlLabel value="사용감 없음" control={<Radio color='primary' />} label="사용감 없음" />
-                      <FormControlLabel value="사용감 적음" control={<Radio color='primary' />} label="사용감 적음" />
-                      <FormControlLabel value="사용감 많음" control={<Radio color='primary' />} label="사용감 많음" />
-                      <FormControlLabel value="고장/파손 상품" control={<Radio color='primary' />} label="고장/파손 상품" />
+                      <FormControlLabel
+                        value='새상품(미사용)'
+                        control={<Radio color='primary' />}
+                        label='새상품(미사용)'
+                      />
+                      <FormControlLabel
+                        value='사용감 없음'
+                        control={<Radio color='primary' />}
+                        label='사용감 없음'
+                      />
+                      <FormControlLabel
+                        value='사용감 적음'
+                        control={<Radio color='primary' />}
+                        label='사용감 적음'
+                      />
+                      <FormControlLabel
+                        value='사용감 많음'
+                        control={<Radio color='primary' />}
+                        label='사용감 많음'
+                      />
+                      <FormControlLabel
+                        value='고장/파손 상품'
+                        control={<Radio color='primary' />}
+                        label='고장/파손 상품'
+                      />
                     </RadioGroup>
                   </FormControl>
-
-
                 </div>
                 <div className={styles.detail__container}>
                   <div className={styles.inner__title}>설명</div>
-                  <textarea name="detailContent" rows={6} className={styles.detail__content}></textarea>
+                  <textarea
+                    name='detailContent'
+                    rows={6}
+                    className={styles.detail__content}
+                  ></textarea>
                 </div>
               </div>
             </div>
@@ -84,11 +107,11 @@ export default function CreateUsedProduct() {
               <div className={styles.num__container}>
                 <div className={styles.inner__title}>가격</div>
                 <div>
-                  <input type="number" value='0' />
+                  <input type='number' value='0' />
                 </div>
               </div>
             </div>
-            
+
             {/* 추가정보 */}
             <div className={styles.create__add__container}>
               <div className={styles.container__title}>추가정보</div>
@@ -96,7 +119,7 @@ export default function CreateUsedProduct() {
                 <div className={styles.num__container}>
                   <div className={styles.inner__title}>수량</div>
                   <div>
-                    <input type="number" value='1' />
+                    <input type='number' value='1' />
                   </div>
                 </div>
                 <div className={styles.transaction__container}>
@@ -105,28 +128,35 @@ export default function CreateUsedProduct() {
                   <FormControl>
                     <RadioGroup
                       row
-                      aria-labelledby="demo-controlled-radio-buttons-group"
-                      name="controlled-radio-buttons-group"
+                      aria-labelledby='demo-controlled-radio-buttons-group'
+                      name='controlled-radio-buttons-group'
                       value={transaction}
                       onChange={handleTransactionChange}
                     >
-                      <FormControlLabel value="가능" control={<Radio color='primary' />} label="가능" />
-                      <FormControlLabel value="불가" control={<Radio color='primary' />} label="불가" />
+                      <FormControlLabel
+                        value='가능'
+                        control={<Radio color='primary' />}
+                        label='가능'
+                      />
+                      <FormControlLabel
+                        value='불가'
+                        control={<Radio color='primary' />}
+                        label='불가'
+                      />
                     </RadioGroup>
                   </FormControl>
-
                 </div>
               </div>
             </div>
-
           </div>
         </div>
 
         {/* Fixed */}
         <div className={styles.fixed__container}>
-          <button type='submit' className={styles.fixed__create}>등록하기</button>
+          <button type='submit' className={styles.fixed__create}>
+            등록하기
+          </button>
         </div>
-      
       </form>
 
       {/* <div style={{marginBottom: '78px'}}>
@@ -135,5 +165,5 @@ export default function CreateUsedProduct() {
 
       <FooterWithMargin />
     </>
-  )
+  );
 }
