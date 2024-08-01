b128f9f79ef26950d1f17ef0983f70cc713c4b7e
j1-y00n <byunjiyun@naver.com>
2024-08-01 19:16:13 +0900

    240801 cart, wishlist, theme수정

--- JJJ-Web/src/pages/Cart.tsx
+++ JJJ-Web/src/pages/Cart.tsx
@@ -1,9 +1,69 @@
 // 변지윤
 // 장바구니
-import React from 'react'
+import React, { ReactNode } from 'react'
 import styles from '../styles/pages/Cart.module.css';
 import Footer from '../components/Footer';
 import exampleImg from '../assets/images/cars.jpg';
+import { Checkbox } from '@mui/material';
+
+
+// Select component
+export const CustomSelect = () => {
+  return (
+    <div className={styles.cart__select}>
+      <div className={styles.select__total}>
+        <Checkbox {...label} color='primary' />
+        <div className={styles.select__total__title}>전체선택</div>
+      </div>
+      <div className={styles.select__delete}>
+        <div className={styles.select__delete__title}>X 선택삭제</div>
+      </div>
+    </div>
+  );
+};
+
+// Product component
+interface CustomProductProps {
+  descClassName?: string;
+  imgClassName?: string;
+}
+
+export const CustomProduct = ({ descClassName, imgClassName }: CustomProductProps) => {
+  return (
+    <div className={`${styles.list__desc} ${descClassName ? descClassName : ''}`}>
+      <CustomButton content='X' className={styles.btn__delete} />
+      <div className={styles.desc__container}>
+        <Checkbox {...label} color='primary' />
+        <img src={exampleImg} alt="상품이미지" className={`${styles.desc__image} ${imgClassName ? imgClassName : ''}`} />
+        <div>
+          <div className={styles.title__font}>0000장난감</div>
+          <div className={styles.context__font}>0000 원</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Button component
+interface CustomButtonProps {
+  content: string;
+  className?: string;
+  onclick?: () => void;
+};
+
+const CustomButton = ({ content, className, onclick }: CustomButtonProps) => {
+  return (
+    <button 
+      className={`${styles.btn} ${className}`}
+    >
+      {content}
+    </button>
+  );
+};
+
+// checkbox label
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
 
 export default function Cart() {
   return (
@@ -13,50 +73,36 @@ export default function Cart() {
           <div className={styles.cart__title}>장바구니</div>
 
           {/* 선택박스 */}
-          <div className={styles.cart__select}>
-            <div className={styles.select__total}>
-              <input type="checkbox" />
-              <div className={styles.select__total__title}>전체선택</div>
-            </div>
-            <div className={styles.select__delete}>
-              <div className={styles.select__delete__title}>X 선택삭제</div>
-            </div>
-          </div>
+          <CustomSelect />
 
           {/* 상품리스트 */}
           <div className={styles.cart__list__container}>
             <div className={styles.list__container__inner}>
-              <div className={styles.list__desc}>
-                <button className={styles.desc__delete__btn}>X</button>
-                <div className={styles.desc__container}>
-                  <input type="checkbox" />
-                  <img src={exampleImg} alt="상품이미지" className={styles.desc__image} />
-                  <div>
-                    <div>상품명</div>
-                    <div>금액</div>
-                  </div>
-                </div>
-              </div>
+
+              <CustomProduct />
+
               <div className={styles.list__quantity}>
-                <div>상품 주문 수량</div>
+                <div className={styles.title__font}>상품 주문 수량</div>
                 <div>
-                  <button>-</button>
-                  <input type="number" />
-                  <button>+</button>
+                  <CustomButton content='-' />
+                  <input type="number" style={{width: '50px', marginRight: '10px', marginLeft: '10px'}} />
+                  <CustomButton content='+' />
                 </div>
               </div>
+
               <div className={styles.list__price}>
-                <div>상품금액</div>
-                <div>0000원</div>
+                <div className={styles.title__font}>상품금액</div>
+                <div className={styles.context__font}>0000 원</div>
               </div>
+
               <div className={styles.list__delivery}>
-                <div>배송비</div>
-                <div>무료</div>
+                <div className={styles.title__font}>배송비</div>
+                <div className={styles.context__font}>무료</div>
               </div>
+
             </div>
-
-
           </div>
+          
         </div>
       </div>
 
@@ -72,5 +118,5 @@ export default function Cart() {
         <Footer />
       </div>
     </>
-  )
-}
+  );
+};
