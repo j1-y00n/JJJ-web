e165b77bd85561ee836dba250c873f71d072881c
Ted <shinsuengju1@gmail.com>
2024-08-19 20:11:06 +0900

    임시 네비바 삭제, Header.tsx 링크 수정

--- JJJ-Web/src/components/Header.tsx
+++ JJJ-Web/src/components/Header.tsx
@@ -1,12 +1,35 @@
 // 박용재
-import styles from "../styles/components/Header.module.css";
-import cars from "../assets/images/cars.jpg";
-import SearchIcon from "@mui/icons-material/Search";
-import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
-import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
-import { NavLink } from "react-router-dom";
+import styles from '../styles/components/Header.module.css';
+import cars from '../assets/images/cars.jpg';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+interface IPages {
+  [key: string]: string;
+}
+
+const links: string[] = [
+  'category/gift',
+  'category/intelligence',
+  'category/fiveSenses',
+  'category/emotion',
+  'category/society',
+  'usedProductList',
+];
+
+const pages: IPages = {
+  'category/gift': '선물',
+  'category/intelligence': '지능발달',
+  'category/fiveSenses': '오감발달',
+  'category/emotion': '정서발달',
+  'category/society': '사회발달',
+  usedProductList: '중고상품',
+};
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <div id={styles.home}>
       <div className={styles.header}>
@@ -22,38 +45,15 @@ export default function Header() {
         <div className={styles.middle_header}>
           <div className={styles.text_container}>
             <ul className={styles.page_title_list}>
-              <li>
-                <a href="" className="present">
-                  선물
-                </a>
-              </li>
-              <li>
-                <a href="" className="intelligent_improvement">
-                  지능발달
-                </a>
-              </li>
-              <li>
-                <a href="" className="senses_improvement">
-                  오감발달
-                </a>
-              </li>
-              <li>
-                <a href="" className="emotion_improvement">
-                  정서발달
-                </a>
-              </li>
-              <li>
-                <a href="" className="society_improvement">
-                  사회발달
-                </a>
-              </li>
-              <li>
-                <NavLink to="/MyUsedProduct">
-                  <a href="" className="used_product">
-                    중고상품
-                  </a>
-                </NavLink>
-              </li>
+              {links.map((link) => (
+                <li
+                  key={link}
+                  className={styles.nav__link}
+                  onClick={() => navigate(`/${link}`)}
+                >
+                  {pages[link]}
+                </li>
+              ))}
             </ul>
           </div>
 
@@ -61,8 +61,8 @@ export default function Header() {
             <div className={styles.box_mytools}>
               <ul className={styles.list_login}>
                 <li>
-                  <NavLink to="/SignIn">
-                    <a href="" className={styles.login}>
+                  <NavLink to='/SignIn'>
+                    <a href='' className={styles.login}>
                       로그인
                     </a>
                   </NavLink>
@@ -71,8 +71,8 @@ export default function Header() {
 
               <ul className={styles.list_signup}>
                 <li>
-                  <NavLink to="/SignUp">
-                    <a href="" className={styles.signup}>
+                  <NavLink to='/SignUp'>
+                    <a href='' className={styles.signup}>
                       회원가입
                     </a>
                   </NavLink>
@@ -83,23 +83,23 @@ export default function Header() {
             <div className={styles.box_icons}>
               <ul className={styles.icon_area}>
                 <li>
-                  <NavLink to="/Search">
-                    <a href="" className={styles.search_icon}>
-                      <SearchIcon sx={{ fontSize: "30px" }} />
+                  <NavLink to='/Search'>
+                    <a href='' className={styles.search_icon}>
+                      <SearchIcon sx={{ fontSize: '30px' }} />
                     </a>
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/Mypage">
-                    <a href="" className={styles.my_page}>
-                      <PermIdentityOutlinedIcon sx={{ fontSize: "30px" }} />
+                  <NavLink to='/Mypage'>
+                    <a href='' className={styles.my_page}>
+                      <PermIdentityOutlinedIcon sx={{ fontSize: '30px' }} />
                     </a>
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/Cart">
-                    <a href="" className={styles.shopping_icon}>
-                      <ShoppingBagOutlinedIcon sx={{ fontSize: "30px" }} />
+                  <NavLink to='/Cart'>
+                    <a href='' className={styles.shopping_icon}>
+                      <ShoppingBagOutlinedIcon sx={{ fontSize: '30px' }} />
                     </a>
                   </NavLink>
                 </li>
@@ -109,6 +109,5 @@ export default function Header() {
         </div>
       </div>
     </div>
-    
   );
 }
