ad844a83c597dcc5a25c8cf813482b94ff9c816a
Ted <shinsuengju1@gmail.com>
2024-08-30 14:01:59 +0900

    usePagination 훅 생성, 상품리스트에 페이지네이션 완성

---
+++ JJJ-Web/src/components/PaginationNav.tsx
@@ -0,0 +1,63 @@
+import { Button } from '@mui/material';
+import styles from '../styles/components/PaginationNav.module.css';
+import useActiveState from '../hooks/useActiveState';
+
+interface PaginationNavProps {
+  itemsPerPage: number;
+  totalItems: number;
+  paginate: (pageNumber: number) => void;
+  currentPage: number;
+}
+
+export default function PaginationNav({
+  itemsPerPage,
+  totalItems,
+  paginate,
+  currentPage,
+}: PaginationNavProps) {
+  const { activeState, handleStateChange } = useActiveState();
+  const pageNumbers = [];
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const currentPageGroup = Math.ceil(currentPage / 5);
+  const startPage = (currentPageGroup - 1) * 5 + 1;
+  const endPage = Math.min(currentPageGroup * 5, totalPages);
+
+  for (let i = startPage; i <= endPage; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav className={styles.pagination}>
+      {startPage > 1 && (
+        <Button
+          onClick={() => paginate(startPage - 5)}
+          className={styles.page__item}
+        >
+          Previous
+        </Button>
+      )}
+      {pageNumbers.map((number) => (
+        <Button
+          key={number}
+          onClick={() => {
+            paginate(number);
+            handleStateChange(number);
+          }}
+          className={`${styles.page__item} ${
+            activeState === number ? 'active' : ''
+          }`}
+        >
+          {number}
+        </Button>
+      ))}
+      {endPage < totalPages && (
+        <Button
+          onClick={() => paginate(startPage + 5)}
+          className={styles.page__item}
+        >
+          Next
+        </Button>
+      )}
+    </nav>
+  );
+}
